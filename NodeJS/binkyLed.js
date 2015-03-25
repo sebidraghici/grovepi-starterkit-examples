a8c7e7dcd2c58d25b5af84ba23c8112751cd5405
André Davis <cubicle.jockey@live.com>
2015-03-24 21:27:02 -0700

    Going to attempt a test
    going to checkin and pull onto raspberry pi

--- NodeJS/binkyLed.js
+++ NodeJS/binkyLed.js
@@ -4,13 +4,26 @@
 	var grovepi = require('node-grovepi');
 	
 	grovepi.init({
-		onInit: function callBack(){
-			grovepi.version(function(response){
-				console.log('Version'.concat(response));
-			});
+		onInit: function callback(){
+			grovepi.run([version(callback), light(callback)]);
 		},
 		onError: function(error){
 			console.log(error);
 		}
 	});
+	
+	function version(callback){
+		grovepi.version(function onVersion(response){
+			console.log('Version'.concat(response));
+			callback();
+		})
+	};
+	
+	function light(callback){
+		var digitalPort = 4;
+		grovepi.light(digitalPort, function onLight(response){
+			console.log('Light'.concat(response));
+			callback();
+		});
+	};
 })()
